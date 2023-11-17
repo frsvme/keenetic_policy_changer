5083220d991bc8e34276aeb8731131819751cfad
Max <maksimka0012@gmail.com>
2023-11-17 22:20:42 +0300

    Uploaded .bat script

--- ChangePolicy.bat
+++ ChangePolicy.bat
@@ -1,9 +1,9 @@
 @echo off
 set PATH=C:\Program Files\Plink;%PATH%
-set keenetic_ip=*ip*
-set user=*user*
-set password=*password*
-set mac=*mac*
-set policyid=*number_of_policy*
+set keenetic_ip=_ip
+set user=_user
+set password=_password
+set mac=_mac
+set policyid=_number_of_policy
 
 plink -ssh %user%@%keenetic_ip% -pw %password% "ip hotspot host %mac% policy Policy%policyid%"
